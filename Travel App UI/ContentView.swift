82f3c76c9ba51050f9b4e2e0b61a18595a523a56
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-02-04 15:16:06 +0530

    Initial Commit

---
+++ Travel App UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Travel App UI
+//
+//  Created by Mahesh Prasad on 04/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Travel App UI/Travel_App_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_App_UIApp.swift
+//  Travel App UI
+//
+//  Created by Mahesh Prasad on 04/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Travel_App_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
